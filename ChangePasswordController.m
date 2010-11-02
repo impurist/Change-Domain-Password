00982f3c0162d8d0d2908c3d6c369b65cfec7d3e
Steven Holloway <impurist@gmail.com>
2010-11-02 13:02:55 +1100

    begin move to TaskWrapperController protocol

--- ChangePasswordController.m
+++ ChangePasswordController.m
@@ -50,31 +50,7 @@
 
 - (IBAction)changePassword:(id)sender;
 {  
-  NSTask *task = [[NSTask alloc] init];
-  NSPipe *pipeOut = [[NSPipe alloc] init];
-  NSPipe *pipeIn = [[NSPipe alloc] init];
-  
-  NSFileHandle *readHandle = [pipeOut fileHandleForReading];
-  NSData *inData = nil;
-  
-  
-  [task setLaunchPath:@"/usr/bin/smbpasswd"];
-  [task setArguments:[NSArray arrayWithObjects:[NSString stringWithFormat:@"-r %@", [[self domainServer] stringValue]],
-                                               [NSString stringWithFormat:@"-U %@", [[self userName] stringValue]],
-                                               nil]];
-  [task setStandardOutput:pipeOut];
-  [task setStandardInput:pipeIn];
-  [task launch];
-  
-  NSString *response = @"";
-  while ((inData = [readHandle availableData]) && [inData length])
-  {
-     NSLog(@"@%",[inData bytes]);
-  }
-  
-  [pipeIn release];
-  [pipeOut release];
-  [task release];
+
 }
 
 @end
